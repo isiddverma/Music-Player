08a0a796f12b5d7513fea8268a28ad957b84722c
Siddharth <114758617+isiddverma@users.noreply.github.com>
2024-09-07 18:12:58 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Main Agar Saamne",
+    artist: "Abhijeet Bhattacharya and Alka Yagnik",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Is Tarah Aashiqui Ka",
+    artist: "Amit Kumar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Kitni Bechain Hoke",
+    artist: "Nadeem-Shravan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Mera Dil Bhi Kitna Pagal Hai",
+    artist: "Kumar Sanu, S. P. Balasubrahmanyam, Alka Yagnik",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Pehli Pehli Baar Mohabbat Ki Hai",
+    artist: "Kumar Sanur Alka Yagnik",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Kash Koi Ladka Mujhe Pyar",
+    artist: "Kumar Sanu",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
